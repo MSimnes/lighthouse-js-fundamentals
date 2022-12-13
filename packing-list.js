473ad4da113ed5f5286e62892ebf5a8abcb12737
Mike Simnes <mike.simnes@gmail.com>
2022-12-13 11:30:16 -0800

    added a while loop

--- packing-list.js
+++ packing-list.js
@@ -1,6 +1,13 @@
 const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
 
 console.log("Kitchen stuff to pack:");
-for (let i = 0; i < packingList.length; i++) {
+// for loop from first example
+// for (let i = 0; i < packingList.length; i++) {
+//   console.log(packingList[i]);
+// }
+
+// for loop changed to while loop
+while (i < packingList.length) {
   console.log(packingList[i]);
+  i++;
 }
